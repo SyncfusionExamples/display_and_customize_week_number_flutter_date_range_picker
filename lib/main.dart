b5ce093c9ac28cb720a4c5a4148fbd54a388317f
arunsiva.rengaraju <arunsiv.rengaraju@syncfusion.com>
2021-10-19 12:57:58 +0530

    Sample added.

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(const WeekNumber());
+
+class WeekNumber extends StatefulWidget {
+  const WeekNumber({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => WeekNumberCustomization();
+}
+
+class WeekNumberCustomization extends State<WeekNumber> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Card(
+        margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+        child: SafeArea(
+          child: SfDateRangePicker(
+            monthViewSettings: const DateRangePickerMonthViewSettings(
+                showWeekNumber: true,
+                weekNumberStyle: DateRangePickerWeekNumberStyle(
+                    textStyle: TextStyle(
+                      color: Colors.amber,
+                    ),
+                    backgroundColor: Color(0xFFDF5E5E))),
+          ),
+        ),
+      )),
+    );
+  }
+}
